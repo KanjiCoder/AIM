7b4bb3889829b5d007d26f70d860e4d71cb1c239
your_name <HeavyMetalCookies@GMail.com>
2022-06-18 01:28:29 -0400

    [sobprint]

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 
+const LIB_URL = require( "url" );
+const LIB_Q_S = require( "node:querystring" );
 
 require( "http" ).createServer( function( i_ask , i_giv ){
 
     var sob ={
         ask : i_ask 
     ,   giv : i_giv
-    ,   url : URL.parse( i_ask.url , 1 ).pathname.toUpperCase()
-    ,   pam : Q_S.parse( i_ask.url , 1 ).query
+    ,   url : LIB_URL.parse( i_ask.url , 1 ).pathname 
+    ,   pam : LIB_Q_S.parse( i_ask.url , 1 ).query
     ,   seg : [ "URL_PATH_SEGMENTS_NOT_LOADED_YET]" ]
     };;
+
+    //:MakeRoutingCaseInsensitive://
+
+    sob.url = sob.url.toUpperCase();
     
     i_giv.setHeader( "Content-Type" , "application/json" );
-    i_giv.end( JSON.stringify( sob );
+    i_giv.end( JSON.stringify( sob ) );
 
 }).listen(process.env.PORT);
