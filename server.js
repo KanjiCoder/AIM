c6740280374b47cf0033b4d0a00005f621eaf05a
your_name <HeavyMetalCookies@GMail.com>
2022-07-02 14:33:50 -0400

    [VIP_AUTO_COMMIT:KanjiCoder]

--- server.js
+++ server.js
@@ -1327,54 +1327,7 @@ if( /** __ARTGIRL__ **/ notnode ){                              // [063]
             );;                                                 // [077]
         }                                                       // [077]
         //:-----------------------------:VBO_IS_AI0:[077]://    // [077]
-        //:VAO_IS_AI1:[078]:-----------------------------://
-        //:                                              ://
-        //:  @s_f@ : Sizeof( float32 ) in bytes          ://
-        //:  @oib@ : Offset In Bytes (to 1st element)    ://
-        //:  @sib@ : Stride In Bytes                     ://
-        //:- - - - - - - - - - - -- - - - - - - - - - - -://
-        {
-            //: #_OPENGL_VS_WEBGL_PART_003_# ://
 
-            //:function_pointers:[078]:------------------://
-
-                let w = wgl; let CVA,BVA,A_P,O_N ;;;;;;;;;
-                CVA =( w.createVertexArray      ).bind(w);
-                BVA =( w.bindVertexArray        ).bind(w);
-                A_P =( w.vertexAttribPointer    ).bind(w);
-                O_N =( w.enableVertexAttribArray).bind(w);
-
-            //:------------------:function_pointers:[078]://
-            //:calculations:[078]:-----------------------://
-
-                let s_f =( 4 /** 4 Bytes Per Float **/ );
-                let typ =( wgl.FLOAT );
-                let n_o =( wgl.FALSE );
-                let sib =( (3+2) * s_f );
-
-                var oib_pos = ( 0 * s_f );  
-                var oib_tex = ( 3 * s_f );  
-
-                let loc_pos = c_artgirl_loc_pos ;
-                let loc_tex = c_artgirl_loc_tex ;
-                
-                if( 0 != loc_pos ){ ERR("[loc_pos]"); };
-                if( 1 != loc_tex ){ ERR("[loc_tex]"); };
-
-            //:-----------------------:calculations:[078]://
-            //:actions:[078]:----------------------------://
-
-                a_g.vao = CVA(         );
-                          BVA( a_g.vao );
-
-                A_P( loc_pos , 3 , typ,n_o,sib , oib_pos ); 
-                A_P( loc_tex , 2 , typ,n_o,sib , oib_tex );
-
-                O_N( loc_pos /**#S_POS#**/ ); 
-                O_N( loc_tex /**#S_TEX#**/ );
-            //:----------------------------:actions:[078]://
-        }
-        //:-----------------------------:VAO_IS_AI1:[078]://
        
 
 
@@ -1504,6 +1457,63 @@ if( /** __ARTGIRL__ **/ notnode ){                              // [063]
             a_g.wgl.clear( a_g.wgl.COLOR_BUFFER_BIT );          // [071][063] 
                                                                 // [071]
         //:---------------:misc_pipeline_setup_code:[071]://    // [071]
+
+
+
+        //:VAO_IS_AI1:[078]:-----------------------------://
+        //:                                              ://
+        //:  @s_f@ : Sizeof( float32 ) in bytes          ://
+        //:  @oib@ : Offset In Bytes (to 1st element)    ://
+        //:  @sib@ : Stride In Bytes                     ://
+        //:- - - - - - - - - - - -- - - - - - - - - - - -://
+        {
+            //: #_OPENGL_VS_WEBGL_PART_003_# ://
+
+            //:function_pointers:[078]:------------------://
+
+                let w = wgl; let CVA,BVA,A_P,O_N ;;;;;;;;;
+                CVA =( w.createVertexArray      ).bind(w);
+                BVA =( w.bindVertexArray        ).bind(w);
+                A_P =( w.vertexAttribPointer    ).bind(w);
+                O_N =( w.enableVertexAttribArray).bind(w);
+
+            //:------------------:function_pointers:[078]://
+            //:calculations:[078]:-----------------------://
+
+                let s_f =( Float32Array.BYTES_PER_ELEMENT );
+                let typ =( wgl.FLOAT );
+                let n_o =( wgl.FALSE );
+                let sib =( (3+2) * s_f );
+
+                var oib_pos = ( 0 * s_f );  
+                var oib_tex = ( 3 * s_f );  
+
+                let loc_pos = c_artgirl_loc_pos ;
+                let loc_tex = c_artgirl_loc_tex ;
+                
+                if( 0 != loc_pos ){ ERR("[loc_pos]"); };
+                if( 1 != loc_tex ){ ERR("[loc_tex]"); };
+                if( 4 !=     s_f ){ ERR("[_4_:s_f]"); };
+
+            //:-----------------------:calculations:[078]://
+            //:actions:[078]:----------------------------://
+
+                a_g.vao = CVA(         );
+                          BVA( a_g.vao );
+
+                A_P( loc_pos , 3 , typ,n_o,sib , oib_pos ); 
+                A_P( loc_tex , 2 , typ,n_o,sib , oib_tex );
+
+                O_N( loc_pos /**#S_POS#**/ ); 
+                O_N( loc_tex /**#S_TEX#**/ );
+            //:----------------------------:actions:[078]://
+        }
+        //:-----------------------------:VAO_IS_AI1:[078]://
+
+
+
+
+
 
     };;                                              //:[063]://
   
